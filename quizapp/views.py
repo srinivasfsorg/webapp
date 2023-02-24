71d9a8191f3913a09a8e7630f7edb6aa4448decb
srinivasmaram2025 <srinivasmaram2025@gmail.com>
2023-02-24 17:15:37 +0530

    add

--- quizapp/views.py
+++ quizapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world, you are at the polls index')
+    return HttpResponse('<h1>Hello world, you are at quizapp index</h1>')
