c1c9ec0308d7a0c52c316fbfca5680763376194f
srinivasmaram2025 <srinivasmaram2025@gmail.com>
2023-02-24 16:26:20 +0530

    added

--- main/urls.py
+++ main/urls.py
@@ -19,8 +19,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('home/', include('quizapp.urls')),
-    path('polls/', include('pollsapp.urls')),
-
+    path(r'', include('quizapp.urls')),
     path('admin/', admin.site.urls),
 ]
